485d3d86008b16afcceb1ac852efd480c6b7be01
FFGFlash <26831072+FFGFlash@users.noreply.github.com>
2025-12-26 14:11:32 -0600

    update exercises module to seperate async and non-async execution

--- @modules/exercises.js
+++ @modules/exercises.js
@@ -1,11 +1,46 @@
-import { measureTimeAsync } from './timing.js'
+import { measureTime, measureTimeAsync } from './timing.js'
+
+let counter = 0
 
 /**
  * Helper function to format lesson output and reduce repetition
  * @param {string} name - The name of lesson
- * @param {() => void | Promise<void>} fn - A callback containing all the execises
+ * @param {() => void} fn - A callback containing all the execises
  */
 export async function lesson(name, fn) {
+  console.log(`=== ${name} Exercises ===\n`)
+
+  measureTime(name, fn)
+
+  console.log('')
+  console.log('=== End of Exercises ===')
+  console.log(`Great job practicing ${name}!`)
+}
+
+/**
+ * Helper function to format exercise output and reduce repetition.
+ * @param {string} name - The name of the exercise.
+ * @param {() => void} fn - A callback containing the code related to the exercise.
+ */
+export function execise(name, fn) {
+  console.log(`Exercise ${++counter}: ${name}`)
+  measureTime(name, () => {
+    try {
+      fn()
+    } catch (e) {
+      console.error(e)
+    } finally {
+      console.log('')
+    }
+  })
+}
+
+/**
+ * Helper function to format lesson output and reduce repetition
+ * @param {string} name - The name of lesson
+ * @param {() => Promise<void>} fn - A callback containing all the execises
+ */
+export async function lessonAsync(name, fn) {
   console.log(`=== ${name} Exercises ===\n`)
 
   await measureTimeAsync(name, fn)
@@ -18,17 +53,17 @@ export async function lesson(name, fn) {
 /**
  * Helper function to format exercise output and reduce repetition.
  * @param {string} name - The name of the exercise.
- * @param {() => void | Promise<void>} fn - A callback containing the code related to the exercise.
+ * @param {() => Promise<void>} fn - A callback containing the code related to the exercise.
  */
-export async function exercise(name, fn) {
-  console.log(`Exercise ${++exercise.counter}: ${name}`)
-  try {
-    await measureTimeAsync(name, fn)
-  } catch (e) {
-    console.error(e)
-  } finally {
-    console.log('')
-  }
+export async function exerciseAsync(name, fn) {
+  console.log(`Exercise ${++counter}: ${name}`)
+  await measureTimeAsync(name, () => {
+    try {
+      fn()
+    } catch (e) {
+      console.error(e)
+    } finally {
+      console.log('')
+    }
+  })
 }
-
-exercise.counter = 0
